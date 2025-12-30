6b84dec26d6e9222079c81e7638d842fd3fbac59
AMT-Technology <des.amt.01@gmail.com>
2025-12-29 20:08:09 -0500

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,14 +1,14 @@
 // Configuración Firebase
-const firebaseConfig = {
-  apiKey: "AIzaSyCEMOAiEhDC-Z5nS4_yB-a1s3T_61PrHF4"
+var firebaseConfig = {
+  apiKey: "AIzaSyCEMOAiEhDC-Z5nS4_yB-a1s3T_61PrHF4",
   authDomain: "appwebs-8b23f.firebaseapp.com",
   projectId: "appwebs-8b23f",
-  storageBucket: "appwebs-8b23f.firebasestorage.com",
+  storageBucket: "appwebs-8b23f.appspot.com",
   messagingSenderId: "986164552498",
-  appId: "1:986164552498:web:04262bde6fa35e5efced2b",
+  appId: "1:986164552498:web:04262bde6fa35e5efced2b"
 };
 
-// Inicializar Firebase
+// Inicializar Firebase (v8)
 firebase.initializeApp(firebaseConfig);
 
 // Servicios
